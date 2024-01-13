629f080fc3bbbf2db71bce1e52a18c94a1c11cf2
yongwookhong <sinabro.yong@gmail.com>
2024-01-13 16:50:12 +0900

    Create code_kata1.cs
    [단순연산]
    
    -두 수의 차
    -두 수의 곱
    -두 수의 몫

---
+++ code_kata1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// 두 수의 차
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
+ 
+// 두 수의 곱
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
+
+// 몫 구하기
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
