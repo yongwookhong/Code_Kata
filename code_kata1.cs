8ea4ff15c8013e8f35fd8b7f4cd45ddf4c6c1dcb
yongwookhong <sinabro.yong@gmail.com>
2024-01-14 11:53:12 +0900

    Update code_kata1.cs
    나이 출력 추가

--- code_kata1.cs
+++ code_kata1.cs
@@ -29,3 +29,16 @@ public class Solution
         return answer;
     }
 }
+
+
+//나이 출력
+//2022년을 기준으로 출생연도를 return하는 솔루션
+//나이는 태어난 연도에 1살이며 1년마다 1씩 증가
+public class Solution
+{
+    public int solution(int age)
+    {
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+}
