cc34aba0707f5e4c1b7f3e8281d8d3cb0117d62c
yongwookhong <sinabro.yong@gmail.com>
2024-01-15 22:14:33 +0900

    Update code_kata1.cs
    숫자 비교하기

--- code_kata1.cs
+++ code_kata1.cs
@@ -42,3 +42,16 @@ public class Solution
         return answer;
     }
 }
+
+//숫자 비교하기
+//두수가 같으면 1 다르면 -1 을 return 하는 solution 함수
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+
+        return num1 == num2 ? 1 : -1;
+    }
+}
+
+
