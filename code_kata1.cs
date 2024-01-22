a2ef4a2830bdef9bf7b582371c30253c1f551d00
yongwookhong <sinabro.yong@gmail.com>
2024-01-23 07:28:59 +0900

    Update code_kata1.cs
    코드카타 짝수와 홀수 추가

--- code_kata1.cs
+++ code_kata1.cs
@@ -178,3 +178,38 @@ public class Solution
         return answer;
     }
 }
+
+
+
+
+// 짝수와 홀수
+//문제 설명
+//정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+//제한 조건
+//num은 int 범위의 정수입니다.
+//0은 짝수입니다.
+
+
+public class Solution
+{
+
+    // solution 함수는 정수형 매개변수 num을 받아 문자열을 반환. 타입은 string
+    public string solution(int num)
+    {
+
+        // 문자열을 저장할 변수 answer를 선언하고 빈 문자열로 초기화
+        string answer = "";
+
+        if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+        else
+        {
+            answer = "Odd";
+        }
+
+        return answer;
+    }
+}
