36bd30e469d91765f890e0188804437969c40e91
yongwookhong <sinabro.yong@gmail.com>
2024-01-24 09:22:47 +0900

    Update code_kata1.cs
    평균값 구하기 추가

--- code_kata1.cs
+++ code_kata1.cs
@@ -213,3 +213,35 @@ public class Solution
         return answer;
     }
 }
+
+
+
+//평균값 구하기
+
+//문제 설명
+//정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+//제한사항
+//arr은 길이 1 이상, 100 이하인 배열입니다.
+//arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+public class Solution
+{
+    public double solution(int[] arr)
+    {
+        double answer = 0;
+
+
+        // 배열의 모든 요소를 더하는 반복문
+        for (int i = 0; i < arr.Length; i++)
+        {
+            answer += arr[i];
+        }
+
+        //배열의 길이로 나누어 평균값 계산
+        answer /= arr.Length;
+
+        //평균값 반환
+        return answer;
+    }
+}
