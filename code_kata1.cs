f906dee7bc970757571f3d3c05e50fbcc07bc1a3
yongwookhong <sinabro.yong@gmail.com>
2024-03-06 09:39:35 +0900

    [feat] 내적

--- code_kata1.cs
+++ code_kata1.cs
@@ -877,3 +877,36 @@ public class Solution
         return answer;
     }
 }
+
+
+// 내적 
+//문제 설명
+//길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+
+//이때, a와 b의 내적은 a[0] * b[0] + a[1] * b[1] + ... +a[n - 1] * b[n - 1] 입니다. (n은 a, b의 길이)
+
+//제한사항
+//a, b의 길이는 1 이상 1,000 이하입니다.
+//a, b의 모든 수는 -1,000 이상 1,000 이하입니다.
+
+
+using System;
+
+public class Solution
+{
+    public int solution(int[] a, int[] b)
+    {
+        // 결과를 저장할 변수 초기화
+        int answer = 1234567890;
+        // 요소들을 더해나갈 변수 초기화
+        int sum = 0;
+        // 두 배열의 각 요소들을 곱하여 누적한 후 저장
+        for (int i = 0; i < a.Length; i++)
+        {
+            sum += a[i] * b[i];
+        }
+
+        answer = sum;
+        return answer;
+    }
+}
